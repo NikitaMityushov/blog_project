5af53a793c49dc0129d5b63cf5992949a6f2dcd0
NikitaMityushov <NikitaMityushov@gmail.com>
2020-11-13 23:02:26 +0300

    master commit 2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -33,5 +35,40 @@ def post_detail(request, year, month, day, post):
                                 publish__year=year, 
                                 publish__month=month,
                                 publish__day=day)
-    context = {'post': post}
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect(post.get_absolute_url())
+    # GET or invalid form
+    comment_form = CommentForm()
+    context = {'post': post, 
+                'comments': comments, 
+                'new_comment': new_comment,
+                'comment_form': comment_form}
     return render(request, 'blog/post/detail.html', context)
+
+# email sending
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            #what this???
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'nikitamityushov@gmail.com', [cd['to'],])
+            sent =  True
+            context = {'post': post, 'form': form, 'sent': sent}
+            return render(request, 'blog/post/share.html', context)
+    # GET or invalid form
+    form = EmailPostForm()
+    context = {'post': post, 'form': form, 'sent': sent}
+    return render(request, 'blog/post/share.html', context)
